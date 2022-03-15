e934f9a4dc95f81bfd6cd3f0d65bcd556d802714
esgaelramos <esgaelramos@gmail.com>
2022-03-15 10:18:45 -0600

    MAYBEno

--- core/settings.py
+++ core/settings.py
@@ -173,12 +173,12 @@ REST_FRAMEWORK = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# #POR DEFECTO ES: 587
-# EMAIL_POST = 587
-# EMAIL_HOST_USER = 'esnefema@gmail.com'
-# EMAIL_HOST_PASSWORD = '*************'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+#POR DEFECTO ES: 587
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'esnefema@gmail.com'
+EMAIL_HOST_PASSWORD = 'NEFEMAgroup$00'
 
 
